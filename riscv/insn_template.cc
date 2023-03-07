6697ec6698175958e32b38226ef857fa6018f757
Mingkai Li <mingkai@ustc.edu>
2023-03-07 07:16:30 +0000

    cap_pc update&check move to intruction exit section; bug fixed

--- riscv/insn_template.cc
+++ riscv/insn_template.cc
@@ -5,30 +5,30 @@
 
 #define check_pc() \
   if (p->get_state()->world == WORLD_SECURE) { \
-    p->get_state()->cap_pc = pc; \
+    p->get_state()->cap_pc.cursor = npc; \
     cap_pc = p->get_state()->cap_pc; \
-    assert(cap_pc.inbound() && cap_pc.readable() && cap_pc.executable() && p->valid_cap(cap_pc)); \
+    assert(cap_pc.inbound() && p->valid_cap(cap_pc)); \
   }
 
 reg_t rv32i_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 32
-  check_pc();
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
   #include "insns/NAME.h"
   trace_opcode(p, OPCODE, insn);
   #undef xlen
+  check_pc();
   return npc;
 }
 
 reg_t rv64i_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 64
-  check_pc();
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
   #include "insns/NAME.h"
   trace_opcode(p, OPCODE, insn);
   #undef xlen
+  check_pc();
   return npc;
 }
 
@@ -38,21 +38,21 @@ reg_t rv64i_NAME(processor_t* p, insn_t insn, reg_t pc)
 reg_t rv32e_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 32
-  check_pc();
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
   #include "insns/NAME.h"
   trace_opcode(p, OPCODE, insn);
   #undef xlen
+  check_pc();
   return npc;
 }
 
 reg_t rv64e_NAME(processor_t* p, insn_t insn, reg_t pc)
 {
   #define xlen 64
-  check_pc();
   reg_t npc = sext_xlen(pc + insn_length(OPCODE));
   #include "insns/NAME.h"
   trace_opcode(p, OPCODE, insn);
   #undef xlen
+  check_pc();
   return npc;
 }
