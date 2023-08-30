1b6cff3bb14eb4d30a7d0f09291c2232764e0ce1
Mingkai Li <mingkai@ustc.edu>
2023-08-30 14:25:47 +0000

    #8 cap_access shouldn't be set before store_rc_update

--- riscv/processor.h
+++ riscv/processor.h
@@ -648,7 +648,7 @@ public:
     return sim->is_pure_capstone();
   }
   /* add for capstone: RC down when overwriting a cap during store.
-   * don't set cap_access before calling this function if addr_is_paddr is set to be false.
+   * don't set cap_access before calling this function.
    */
   void store_update_rc(uint64_t addr, bool addr_is_paddr = true);
 
